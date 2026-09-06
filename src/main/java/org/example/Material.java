f000ad24318c5e8a70f468bd249f83e05d6a6bc4
Valeria <vporras847@gmail.com>
2026-09-05 22:41:36 -0500

    se agrego el contador global "cantidadCreados" para contar el total  material creado

--- src/main/java/org/example/Material.java
+++ src/main/java/org/example/Material.java
@@ -3,23 +3,24 @@ package org.example;
 import java.time.LocalDate;
 
 public abstract class Material {
-    private String idMaterial;
-    private String titulo;
-    private  String autor;
-    private String editorial;
-    private LocalDate fechaPublicaion;
-    private String idioma;
+    private static int CantidadCreados=0;
+
+    protected String idMaterial;
+    protected String titulo;
+    protected String autor;
+    protected String editorial;
+    protected LocalDate fechaPublicaion;
+    protected String idioma;
 
     public Material(String idMaterial,String titulo, String autor, String editorial, LocalDate fechaPublicacion, String idioma){
         this.idMaterial = idMaterial;
         this.titulo = titulo;
-
         this.autor = autor;
         this.editorial = editorial;
         this.fechaPublicaion = fechaPublicacion;
         this.idioma = idioma;
-
     }
+
     public String getIdMaterial(){
         return idMaterial;
     }
@@ -37,6 +38,10 @@ public abstract class Material {
     }
     public String getIdioma(){
         return idioma;
+    }
+
+    public static int getCantidadCreados (){
+        return CantidadCreados;
     }
 
 }
