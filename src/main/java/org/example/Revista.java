135f9bcfa94eaec239c990a27d9af662a3e63f80
Valeria <vporras847@gmail.com>
2026-09-06 00:06:15 -0500

    se implementó la interface Prestable a la clase Revista

--- src/main/java/org/example/Revista.java
+++ src/main/java/org/example/Revista.java
@@ -3,21 +3,31 @@ package org.example;
 import java.time.LocalDate;
 import java.util.Date;
 
-public class Revista extends Material {
+public class Revista extends Material implements Prestable {
     private String periodicidad;
     private String volumen;
     private  String numero;
     private int cantidadDisponible;
-    private  String estado;
 
-    public  Revista(String idMaterial, String nombre, String tipo, String autor, String editorial, LocalDate fechaPublicacion, String idioma, int cantidadDisponible, String estado, String periodicidad, String volumen, String numero){
-        super(idMaterial,nombre, tipo, autor, editorial, fechaPublicacion, idioma);
+    public  Revista(String idMaterial, String titulo, String autor, String editorial, LocalDate fechaPublicacion, String idioma, String periodicidad, String volumen, String numero, int cantidadDisponible){
+        super(idMaterial,titulo, autor, editorial, fechaPublicacion, idioma);
         this.periodicidad = periodicidad;
         this.volumen = volumen;
         this.numero = numero;
         this.cantidadDisponible = cantidadDisponible;
-        this.estado = estado;
-        
+    }
+    @Override
+    public void prestado() {
+        if (cantidadDisponible>0) cantidadDisponible--;
     }
 
+    @Override
+    public void devuelto() {
+        if(cantidadDisponible>0) cantidadDisponible++;
+    }
+
+    @Override
+    public boolean estaDisponible() {
+        return (cantidadDisponible>0);
+    }
 }
